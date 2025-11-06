c8fecd761f0e8590a740cee4d422dd30ffb60094
Shourya <shourya124sensei@gmail.com>
2025-11-06 16:57:23 +0530

    Added route for jcafe

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import StudentDashboard from './pages/StudentsDashboard/Dashboard-students.jsx';
 import StudentTimetable from './pages/StudentsDashboard/StudentTimetable.jsx';
 import StudentAttendance from './pages/StudentsDashboard/StudentAttendance.jsx';
 import StudentNotifications from './pages/StudentsDashboard/StudentNotification.jsx';
+import JCafe from './pages/StudentsDashboard/JCafe.jsx'
 
 function App() {
     const { currentUser } = useSelector(state => state.user);
@@ -21,6 +22,7 @@ function App() {
         <Route path='/student/timetable' element={<StudentTimetable />} />
         <Route path='/student/attendance' element={<StudentAttendance />} />
         <Route path='/student/notifications' element={<StudentNotifications />} />
+        <Route path='/student/jcafe' element={<JCafe />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<Signup />} />
         {currentUser ? (
