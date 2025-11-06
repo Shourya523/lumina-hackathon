de2ee00237bbc17bb94d35538e3eee2d063595e9
Ansh <144668849+Mercy036@users.noreply.github.com>
2025-11-06 17:55:55 +0530

    jcafe fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,38 @@
-import Signup from './pages/SignUp.jsx'
-import ProfilePage from './pages/ProfilePage.jsx'
-import SignIn from './pages/SignIn.jsx'
-import { useSelector } from 'react-redux'
-import './index.css'
+import Signup from './pages/SignUp.jsx';
+import ProfilePage from './pages/ProfilePage.jsx';
+import SignIn from './pages/SignIn.jsx';
+import { useSelector } from 'react-redux';
+import './index.css';
 import HomePage from './pages/HomePage/HomePage.jsx';
 import { Routes, Route } from "react-router-dom";
 import StudentDashboard from './pages/StudentsDashboard/Dashboard-students.jsx';
 import StudentTimetable from './pages/StudentsDashboard/StudentTimetable.jsx';
 import StudentAttendance from './pages/StudentsDashboard/StudentAttendance.jsx';
 import StudentNotifications from './pages/StudentsDashboard/StudentNotification.jsx';
-import JCafe from './pages/StudentsDashboard/JCafe.jsx'
+import JCafe from './pages/StudentsDashboard/JCafe.jsx';
 
 function App() {
-    const { currentUser } = useSelector(state => state.user);
+  const { currentUser } = useSelector(state => state.user);
 
   return (
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/student-dashboard' element={<StudentDashboard />} />
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/student-dashboard' element={<StudentDashboard />} />
+      <Route path='/student/jcafe' element={<JCafe />} />
+      <Route path='/student/timetable' element={<StudentTimetable />} />
+      <Route path='/student/attendance' element={<StudentAttendance />} />
+      <Route path='/student/notifications' element={<StudentNotifications />} />
 
-        <Route path='/student/timetable' element={<StudentTimetable />} />
-        <Route path='/student/attendance' element={<StudentAttendance />} />
-        <Route path='/student/notifications' element={<StudentNotifications />} />
-        <Route path='/student/jcafe' element={<JCafe />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<Signup />} />
-        {currentUser ? (
-          <Route path="/profile" element={<ProfilePage />} />
-        ) : (
-          <Route path="/" element={<HomePage />} />
-        )}
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<Signup />} />
 
-      </Routes>
-  )
+      {currentUser ? (
+        <Route path="/profile" element={<ProfilePage />} />
+      ) : (
+        <Route path="/" element={<HomePage />} />
+      )}
+    </Routes>
+  );
 }
 
-export default App
+export default App;
